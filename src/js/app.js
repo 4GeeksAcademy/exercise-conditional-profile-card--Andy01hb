f321d00c073e63004e0725791d852b4dfdab4df0
Andy01hb <128887067+Andy01hb@users.noreply.github.com>
2024-08-10 22:56:48 +0000

    correcciones redes sociales hechas

--- src/js/app.js
+++ src/js/app.js
@@ -1,34 +1,28 @@
 import { right } from "@popperjs/core";
 import "../style/index.css";
 
-/**
- *  EDIT ONLY INSIDE THIS RENDER FUNCTION
- *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should show the cover image
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "right", // social media bar position (left or right)
-        
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
-
-        name: null,
-        lastName: null,
-        role: null,
-        country: null,
-        city: null
-    }
- */
 function render(variables = {}) {
   console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+
+  // Aquí creamos las URLs de búsqueda para cada red social
+  const twitterSearch = variables.twitter
+    ? `https://twitter.com/search?q=${encodeURIComponent(variables.twitter)}`
+    : "https://x.com/4geeksacademyES";
+  const githubSearch = variables.github
+    ? `https://github.com/search?q=${encodeURIComponent(variables.github)}`
+    : "https://github.com/4GeeksAcademy";
+  const linkedinSearch = variables.linkedin
+    ? `https://www.linkedin.com/search/results/all/?keywords=${encodeURIComponent(
+        variables.linkedin
+      )}`
+    : "https://www.linkedin.com/school/4geeks-academy-latino/";
+  const instagramSearch = variables.instagram
+    ? `https://www.instagram.com/explore/tags/${encodeURIComponent(
+        variables.instagram
+      )}/`
+    : "https://www.instagram.com/4geeksacademylatam/";
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
@@ -42,44 +36,20 @@ function render(variables = {}) {
     variables.country ? variables.country : "Country"
   }</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="${
-              variables.twitter
-                ? variables.twitter
-                : "https://x.com/4geeksacademyES"
-            }" target="_blank"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="${
-              variables.github
-                ? variables.github
-                : "https://github.com/4GeeksAcademy"
-            }" target="_blank"><i class="fab fa-github"></i></a></li>
-            <li><a href="${
-              variables.linkedin
-                ? variables.linkedin
-                : "https://www.linkedin.com/school/4geeks-academy-latino/"
-            }" target="_blank"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="${
-              variables.instagram
-                ? variables.instagram
-                : "https://www.instagram.com/4geeksacademylatam/"
-            }" target="_blank"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="${twitterSearch}" target="_blank"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${githubSearch}" target="_blank"><i class="fab fa-github"></i></a></li>
+            <li><a href="${linkedinSearch}" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${instagramSearch}" target="_blank"><i class="fab fa-instagram"></i></a></li>
           </ul>
   </div>`;
 }
 
-/**
- * Don't change any of the lines below, here is where we do the logic for the dropdowns
- */
 window.onload = function() {
   window.variables = {
-    // if includeCover is true the algorithm should show the cover image
     includeCover: true,
-    // this is the image's url that will be used as a background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
-    // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
-    // social media bar position (left or right)
     socialMediaPosition: "position-left",
-    // social media usernames
     twitter: null,
     github: null,
     linkedin: null,
@@ -94,8 +64,7 @@ window.onload = function() {
 
   document.querySelectorAll(".picker").forEach(function(elm) {
     elm.addEventListener("change", function(e) {
-      // <- add a listener to every input
-      const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
+      const attribute = e.target.getAttribute("for");
       let values = {};
       values[attribute] =
         this.value == "" || this.value == "null"
